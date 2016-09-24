b37de9c0a53b1c8f18e606173021cc246e1de1cc
Maksim V <inre.storm@gmail.com>
2016-09-24 16:54:48 +0300

    Improve usage examples

--- examples/pub.js
+++ examples/pub.js
@@ -1,11 +1,29 @@
+/* eslint no-console: off */
 
-var grpc = require('grpc')
-var pubsub = grpc.load('definitions/pubsub/broker.proto').pubsub
+const grpc = require('grpc')
 
-var client = new pubsub.Broker('localhost:50051',
+const pubsub = grpc.load('definitions/pubsub/broker.proto').pubsub
+
+const topic = process.argv[2]
+const payloads = process.argv.slice(3)
+
+if (typeof (topic) !== 'string' || payloads.length === 0) {
+  console.log('Usage npm run pub topic payload [payload...]')
+  process.exit(0)
+}
+
+const client = new pubsub.Broker('localhost:50051',
                                      grpc.credentials.createInsecure())
 
-client.publish({ topic:'test1', messages: [{data: 'test_payload'}]}, function(err, response) {
-  console.log(err)
-  console.log(response)
+const messages = payloads.map((payload) => ({ data: payload }))
+
+const message = {
+  topic,
+  messages,
+}
+
+client.publish(message, (err) => {
+  if (err) {
+    console.log(err)
+  }
 })
--- examples/sub.js
+++ examples/sub.js
@@ -2,9 +2,16 @@
 var grpc = require('grpc')
 var pubsub = grpc.load('definitions/pubsub/broker.proto').pubsub
 
+const topics = process.argv.slice(2)
+
+if (topics.length === 0) {
+  console.log('Usage npm run sub topic [topic...]')
+  process.exit(0)
+}
+
 var client = new pubsub.Broker('localhost:50051',
                                      grpc.credentials.createInsecure())
-                                     
-call = client.subscribe({topics: ['test1'], maxMessages: 1})
+
+call = client.subscribe({topics: topics, maxMessages: 1})
 call.on('data', (data) => console.log(data))
 call.on('end', () => console.log('end'))
