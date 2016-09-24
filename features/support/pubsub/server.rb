4d0b8e1525747851653bddceb3d6475745a9bd2b
Maksim V <inre.storm@gmail.com>
2016-09-24 15:06:33 +0300

    Run test correctly

--- features/support/pubsub/server.rb
+++ features/support/pubsub/server.rb
@@ -1,10 +1,10 @@
 module Pubsub
   class Server
     def initialize(debug: false)
-      @io = IO.popen(debug ? 'DEBUG=pubsub:* npm start' : 'npm start')
+      @io = IO.popen(debug ? 'DEBUG=pubsub:* node runner.js' : 'node runner.js')
       sleep 4 # FIXME: Should we watch the pid file or something else
     end
-
+    
     def finalize
       unless @io.nil?
         Process.kill('TERM', @io.pid) # FIXME: QUIT doesn't help
